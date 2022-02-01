717efcedd808a89887b44425fd25a92321c3e2fd
Murad Awwad <muradbaniawwad@gmail.com>
2022-02-01 20:43:55 +0200

    creating view model in the main activity.

--- app/src/main/java/com/murad/introtodi/ui/MainActivity.kt
+++ app/src/main/java/com/murad/introtodi/ui/MainActivity.kt
@@ -5,8 +5,16 @@ import android.os.Bundle
 import com.murad.introtodi.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var viewModel:MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = MainViewModel(this)
+
+
+
     }
 }
